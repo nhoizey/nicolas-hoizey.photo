f4eb0f61ee4668f45e3ed90ff943c5cb62b6e812
Nicolas Hoizey <nhoizey@clever-age.com>
2021-05-17 00:58:15 +0200

    Fix typo

--- src/_11ty/filters/collection.js
+++ src/_11ty/filters/collection.js
@@ -1,23 +1,20 @@
+// Inspired by @tylersticka
+// https://github.com/11ty/eleventy/issues/399#issuecomment-466514166
+
 module.exports = {
-  photos_here: (collection, url) => {
-    return collection.filter((page) => {
+  photos_here: (collection, url) =>
+    collection.filter((page) => {
       const r = new RegExp(`^${url}[^/]+\/$`);
       return page.data.photo !== undefined && r.test(page.url);
-    });
-  },
-  photos_bellow: (collection, url) => {
-    // Inspired by @tylersticka
-    // https://github.com/11ty/eleventy/issues/399#issuecomment-466514166
-    return collection.filter((page) => {
+    }),
+  photos_below: (collection, url) =>
+    collection.filter((page) => {
       const r = new RegExp(`^${url}[^/]+\/([^/]+\/)+$`);
       return page.data.photo !== undefined && r.test(page.url);
-    });
-  },
-  subgalleries: (collection, url) => {
-    const sub = collection.filter((page) => {
+    }),
+  subgalleries: (collection, url) =>
+    collection.filter((page) => {
       const r = new RegExp(`^${url}[^/]+\/$`);
       return page.data.photo === undefined && r.test(page.url);
-    });
-    return sub;
-  },
+    }),
 };
