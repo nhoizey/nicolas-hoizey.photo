499b58ad660491a7123a99f792b3ded867c87db1
Nicolas Hoizey <nhoizey@clever-age.com>
2021-05-24 19:58:52 +0200

    Enhance logo loading

---
+++ assets/js/critical.js
@@ -0,0 +1,7 @@
+(function (window) {
+  // Change the `no-js` class on <html> to `js`
+  window.document.documentElement.className = window.document.documentElement.className.replace(
+    'no-js',
+    'js'
+  );
+})(window);
