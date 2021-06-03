09a1856826fb58ae799684a1d52921c4ea0c59c3
Nicolas Hoizey <nhoizey@clever-age.com>
2021-06-03 09:51:41 +0200

    Add unicode range

--- _scripts/subset-font.sh
+++ _scripts/subset-font.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-glyphhanger https://nicolas-hoizey.photo/tools/subset.html --subset=./assets/font/*.ttf --formats=woff2,woff
+glyphhanger https://nicolas-hoizey.photo/tools/subset.html --subset=./assets/font/*.ttf --formats=woff2,woff --css
 mv ./assets/font/*-subset.woff* ./src/ui/fonts/
