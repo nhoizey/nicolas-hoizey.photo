ef1ff17316b0355622dcbacf582a535907b91046
Nicolas Hoizey <nhoizey@clever-age.com>
2021-06-03 09:19:56 +0200

    Use the HTML version

--- _scripts/subset-font.sh
+++ _scripts/subset-font.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-glyphhanger https://nicolas-hoizey.photo/tools/subset-raw/ --subset=./assets/font/*.ttf --formats=woff2,woff
+glyphhanger https://nicolas-hoizey.photo/tools/subset.html --subset=./assets/font/*.ttf --formats=woff2,woff
 mv ./assets/font/*-subset.woff* ./src/ui/fonts/
