cc16d2147ff9e025b8ae86b516aa3f9b85e8bc54
Nicolas Hoizey <nhoizey@clever-age.com>
2021-06-05 00:12:36 +0200

    Use local version to get font subset

--- _scripts/subset-font.sh
+++ _scripts/subset-font.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-glyphhanger https://nicolas-hoizey.photo/tools/subset.html --subset=./assets/font/*.ttf --formats=woff2,woff --css
+glyphhanger http://localhost:8080/tools/subset.html --subset=./assets/font/*.ttf --formats=woff2,woff --css
 mv ./assets/font/*-subset.woff* ./src/ui/fonts/
