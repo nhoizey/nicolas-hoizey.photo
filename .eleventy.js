fd9fc0d813d3b842dc4adee036513a9e36af1593
Nicolas Hoizey <nhoizey@clever-age.com>
2021-06-14 23:24:06 +0200

    Disable responsive-imager temporarily

--- .eleventy.js
+++ .eleventy.js
@@ -60,14 +60,14 @@ module.exports = function (eleventyConfig) {
   // Transforms
   // ------------------------------------------------------------------------
 
-  if (process.env.NODE_ENV === 'production') {
-    const imagesResponsiver = require('eleventy-plugin-images-responsiver');
-    const imagesResponsiverConfig = require(path.join(
-      __dirname,
-      config.dir.src,
-      '_11ty/images-responsiver-config.js'
-    ));
-    eleventyConfig.addPlugin(imagesResponsiver, imagesResponsiverConfig);
+  // if (process.env.NODE_ENV === 'production') {
+  //   const imagesResponsiver = require('eleventy-plugin-images-responsiver');
+  //   const imagesResponsiverConfig = require(path.join(
+  //     __dirname,
+  //     config.dir.src,
+  //     '_11ty/images-responsiver-config.js'
+  //   ));
+  //   eleventyConfig.addPlugin(imagesResponsiver, imagesResponsiverConfig);
 
     const htmlMinTransform = require(path.join(
       __dirname,
