19d95c2eef7c28105df7b8731aa1ed7aba706e32
Nicolas Hoizey <nhoizey@clever-age.com>
2021-06-14 14:10:51 +0200

    Show mapbox map on photo page

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -1,5 +1,5 @@
 import res from 'res';
-// import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 
 (function (window) {
   // get device pixel ratio in dppx
@@ -13,18 +13,32 @@ import res from 'res';
   );
 
   // Load Mapbox map if necessary
-  // const mapElementId = 'map';
-  // const mapElement = window.document.querySelector(`#${mapElementId}`);
-  // const mapLatitude = mapElement.dataset.latitude;
-  // const mapLongitude = mapElement.dataset.longitude;
+  const mapElementId = 'map';
+  const mapElement = window.document.querySelector(`#${mapElementId}`);
+  const mapLatitude = mapElement.dataset.latitude;
+  const mapLongitude = mapElement.dataset.longitude;
 
-  // if (mapElement) {
-  //   mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
-  //   let map = new mapboxgl.Map({
-  //     container: mapElementId,
-  //     style: 'mapbox://styles/mapbox/dark-v10',
-  //     center: [mapLongitude, mapLatitude],
-  //     zoom: 10,
-  //   });
-  // }
+  if (mapElement) {
+    mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+    let map = new mapboxgl.Map({
+      container: mapElementId,
+      style: 'mapbox://styles/nhoizey/cjese953v0peh2spcgq64caff',
+      center: [mapLongitude, mapLatitude],
+      zoom: 6,
+      attributionControl: false,
+    });
+    map.dragRotate.disable();
+    map.touchZoomRotate.disableRotation();
+    var marker = new mapboxgl.Marker({
+      color: '#a081c0',
+      draggable: false,
+    })
+      .setLngLat([mapLongitude, mapLatitude])
+      .addTo(map);
+    map.addControl(
+      new mapboxgl.NavigationControl({ showCompass: false }),
+      'top-right'
+    );
+    map.addControl(new mapboxgl.FullscreenControl());
+  }
 })(window);
