7e67372fe75de827dc9a3f406394b04344593572
Nicolas Hoizey <nhoizey@clever-age.com>
2021-07-06 19:38:53 +0200

    Use logical properties in source Sass

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require('autoprefixer'), require('cssnano')],
+  plugins: [
+    require('postcss-logical'),
+    require('autoprefixer'),
+    require('cssnano'),
+  ],
 };
