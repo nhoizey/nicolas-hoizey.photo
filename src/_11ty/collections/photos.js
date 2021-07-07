ac732caff23263c85a2ba45ece70b4f2fc982572
Nicolas Hoizey <nhoizey@clever-age.com>
2021-07-07 19:21:36 +0200

    Select by glob pattern

--- src/_11ty/collections/photos.js
+++ src/_11ty/collections/photos.js
@@ -5,8 +5,7 @@ const sortOrderThenAlpha = require('../_utils/sort-order-then-alpha');
 module.exports = {
   photos: (collection) =>
     collection
-      .getAll()
-      .filter((item) => item.url.match(/^\/photos\/[^/]+\//))
+      .getFilteredByGlob('src/photos/*/index.md')
       .sort((a, b) => b.date - a.date),
   galleries: (collection) =>
     collection
