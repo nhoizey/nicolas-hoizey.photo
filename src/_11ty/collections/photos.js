890dab36e130feae0d80091557d5af2b46961b84
Nicolas Hoizey <nhoizey@clever-age.com>
2021-07-08 00:41:51 +0200

    Fix galleries collections

--- src/_11ty/collections/photos.js
+++ src/_11ty/collections/photos.js
@@ -1,15 +1,28 @@
-// Add a "photos" collection with all photos
+const getPhotoData = require('../_utils/photo-data');
 
 const sortOrderThenAlpha = require('../_utils/sort-order-then-alpha');
 
 module.exports = {
-  photos: (collection) =>
+  photos_in_galleries: (collection) =>
+    collection
+      .getFilteredByGlob([
+        'src/travels/**/*.md',
+        'src/wanderings/**/*.md',
+        'src/portraits/**/*.md',
+      ])
+      .filter((item) => !item.filePathStem.endsWith('/index'))
+      .sort((a, b) => b.data.date - a.data.date),
+  all_photos: (collection) =>
     collection
       .getFilteredByGlob('src/photos/*/index.md')
       .sort((a, b) => b.date - a.date),
   galleries: (collection) =>
     collection
-      .getAll()
-      .filter((item) => item.url.match(/^\/(travels|wanderings|portraits)\//))
+      .getFilteredByGlob([
+        'src/travels/**/*.md',
+        'src/wanderings/**/*.md',
+        'src/portraits/**/*.md',
+      ])
+      .filter((item) => item.filePathStem.endsWith('/index'))
       .sort((a, b) => sortOrderThenAlpha(a, b)),
 };
