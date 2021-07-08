7c497946700c5e35f56027bbe214c0db5ffe04bd
Nicolas Hoizey <nhoizey@clever-age.com>
2021-07-08 19:11:11 +0200

    List photos not yet used in galleries

--- src/_11ty/collections/photos.js
+++ src/_11ty/collections/photos.js
@@ -1,8 +1,10 @@
-const getPhotoData = require('../_utils/photo-data');
-
 const sortOrderThenAlpha = require('../_utils/sort-order-then-alpha');
 
 module.exports = {
+  all_photos: (collection) =>
+    collection
+      .getFilteredByGlob('src/photos/*/index.md')
+      .sort((a, b) => b.date - a.date),
   photos_in_galleries: (collection) =>
     collection
       .getFilteredByGlob([
@@ -12,10 +14,20 @@ module.exports = {
       ])
       .filter((item) => !item.filePathStem.endsWith('/index'))
       .sort((a, b) => b.data.date - a.data.date),
-  all_photos: (collection) =>
-    collection
+  photos_not_in_galleries: (collection) => {
+    const used_photos = collection
+      .getFilteredByGlob([
+        'src/travels/**/*.md',
+        'src/wanderings/**/*.md',
+        'src/portraits/**/*.md',
+      ])
+      .filter((item) => !item.filePathStem.endsWith('/index'))
+      .map((item) => item.fileSlug);
+    return collection
       .getFilteredByGlob('src/photos/*/index.md')
-      .sort((a, b) => b.date - a.date),
+      .filter((item) => !used_photos.includes(item.fileSlug))
+      .sort((a, b) => b.data.date - a.data.date);
+  },
   galleries: (collection) =>
     collection
       .getFilteredByGlob([
