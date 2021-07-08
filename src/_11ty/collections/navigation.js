0078ca63dff1c35dd01ac464e057b581bf0f7124
Nicolas Hoizey <nhoizey@clever-age.com>
2021-07-08 15:43:45 +0200

    Search for nunjucks templates also

--- src/_11ty/collections/navigation.js
+++ src/_11ty/collections/navigation.js
@@ -5,7 +5,7 @@ const sortOrderThenAlpha = require('../_utils/sort-order-then-alpha');
 module.exports = {
   navigation: (collection) =>
     collection
-      .getFilteredByGlob('src/*/index.md')
+      .getFilteredByGlob('src/*/index.*')
       .filter((item) => 'nav' in item.data && 'order' in item.data.nav)
       .sort((a, b) => sortOrderThenAlpha(a, b)),
   navigation2: (collection) =>
