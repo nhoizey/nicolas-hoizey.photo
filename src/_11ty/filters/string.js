eefcd8f6e9603acdb61be0df4599856c764b9bb4
Nicolas Hoizey <nhoizey@clever-age.com>
2021-10-19 01:08:00 +0200

    Add opengraph image to photo pages

--- src/_11ty/filters/string.js
+++ src/_11ty/filters/string.js
@@ -2,4 +2,7 @@ const slugifyString = require('../_utils/slugify');
 
 module.exports = {
   slugify: (string) => slugifyString(string),
+  base64: (string) => {
+    return Buffer.from(string).toString('base64');
+  },
 };
