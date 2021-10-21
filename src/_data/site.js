9ee750d0cf275366e484880b6b22a37fff68fa6a
Nicolas Hoizey <nhoizey@clever-age.com>
2021-10-21 14:16:55 +0200

    Refactor opengraph images

--- src/_data/site.js
+++ src/_data/site.js
@@ -15,4 +15,29 @@ module.exports = {
     twitter: 'nhoizey',
     facebook: 'nhoizey',
   },
+  opengraph: {
+    background: 'resources/nho-photo-opengraph-background',
+    prefix:
+      'https://res.cloudinary.com/nho/image/upload/w_1200,h_630,c_fill,q_auto,f_auto',
+    title: {
+      left: {
+        before: 'w_480,c_fit,co_rgb:f7f5fa,l_text:Roboto%20Condensed_50',
+        after: 'fl_layer_apply,g_south_west,y_384,x_48',
+      },
+      top: {
+        before: 'w_1104,c_fit,co_rgb:f7f5fa,l_text:Roboto%20Condensed_50',
+        after: 'fl_layer_apply,g_north_west,y_96,x_48',
+      },
+    },
+    subtitle: {
+      left: {
+        before: 'w_480,c_fit,co_rgb:bda7d3,l_text:Roboto%20Condensed_30',
+        after: 'fl_layer_apply,g_south_west,y_288,x_48',
+      },
+      top: {
+        before: 'w_1104,c_fit,co_rgb:bda7d3,l_text:Roboto%20Condensed_30',
+        after: 'fl_layer_apply,g_south_west,y_288,x_48',
+      },
+    },
+  },
 };
