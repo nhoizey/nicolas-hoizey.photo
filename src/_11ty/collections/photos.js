9976d1502e29380746e5a0f00660c7b61ca53bb9
Nicolas Hoizey <nhoizey@clever-age.com>
2022-01-06 13:34:11 +0100

    Ignore some photos

--- src/_11ty/collections/photos.js
+++ src/_11ty/collections/photos.js
@@ -1,5 +1,12 @@
 const sortOrderThenAlpha = require('../_utils/sort-order-then-alpha');
 
+const ignoredSlugs = [
+  'monochrome',
+  'grignotage',
+  'gouter-d-enfant',
+  'gourmandise',
+];
+
 const allPhotosGlob = 'src/photos/*/index.md';
 const usedPhotosGlob = 'src/galleries/**/*.md';
 const galleriesGlob = 'src/galleries/**/index.md';
@@ -19,6 +26,7 @@ module.exports = {
       .map((item) => item.fileSlug);
     return collection
       .getFilteredByGlob(allPhotosGlob)
+      .filter((item) => !ignoredSlugs.includes(item.fileSlug))
       .filter((item) => !used_photos.includes(item.fileSlug))
       .sort((a, b) => b.data.date - a.data.date);
   },
