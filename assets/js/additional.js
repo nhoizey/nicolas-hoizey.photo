62230cef04255262b10075f60fb2aaebe745a293
Nicolas Hoizey <nhoizey@clever-age.com>
2022-01-25 14:38:10 +0100

    Add Google's web-vital library to track LCP and CLS in Matomo

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -1,4 +1,5 @@
 import res from 'res';
+import { getLCP, getCLS } from 'web-vitals/base';
 
 (function (window) {
   // get device pixel ratio in dppx
@@ -11,3 +12,21 @@ import res from 'res';
     window.innerWidth || 0
   );
 })(window);
+
+function sendToMatomo({ name, value }) {
+  console.dir({ name, value });
+
+  if ('_paq' in window) console.log('ok');
+
+  switch (name) {
+    case 'LCP':
+      // _paq.push(['setCustomVariable', 1, 'LCP', Math.round(value), 'page']);
+      break;
+    case 'CLS':
+      // _paq.push(['setCustomVariable', 2, 'CLS', Math.round(value * 1000), 'page']);
+      break;
+  }
+}
+
+getCLS(sendToMatomo);
+getLCP(sendToMatomo);
