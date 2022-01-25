63475ded9e861f884879e9e5b07972e0188afbdd
Nicolas Hoizey <nhoizey@clever-age.com>
2022-01-25 14:58:02 +0100

    Add custom dimensions to Matomo reporting

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -4,6 +4,7 @@ import { getLCP, getCLS } from 'web-vitals/base';
 (function (window) {
   // get device pixel ratio in dppx
   let screen_density = res.dppx();
+  _paq.push(['setCustomVariable', 3, 'screen_density', screen_density, 'page']);
 
   // get viewport width
   // http://stackoverflow.com/a/8876069/717195
@@ -11,20 +12,28 @@ import { getLCP, getCLS } from 'web-vitals/base';
     window.document.documentElement.clientWidth,
     window.innerWidth || 0
   );
+  _paq.push(['setCustomVariable', 4, 'viewport_width', viewport_width, 'page']);
 })(window);
 
 function sendToMatomo({ name, value }) {
   console.dir({ name, value });
 
-  if ('_paq' in window) console.log('ok');
-
-  switch (name) {
-    case 'LCP':
-      // _paq.push(['setCustomVariable', 1, 'LCP', Math.round(value), 'page']);
-      break;
-    case 'CLS':
-      // _paq.push(['setCustomVariable', 2, 'CLS', Math.round(value * 1000), 'page']);
-      break;
+  if ('_paq' in window) {
+    // Matomo is available
+    switch (name) {
+      case 'LCP':
+        _paq.push(['setCustomVariable', 1, 'LCP', Math.round(value), 'page']);
+        break;
+      case 'CLS':
+        _paq.push([
+          'setCustomVariable',
+          2,
+          'CLS',
+          Math.round(value * 1000),
+          'page',
+        ]);
+        break;
+    }
   }
 }
 
