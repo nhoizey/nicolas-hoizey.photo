ba7c46a35bff1069e5cd0fa5c5356737a5e9b37c
Nicolas Hoizey <nhoizey@clever-age.com>
2022-01-28 20:15:44 +0100

    Add a script to add dates to photos, from Git log

---
+++ _scripts/add-dates.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+find src/galleries -name "*.md" | while read fname; do
+  if [[ ! "$fname" =~ .*"index.md" ]]
+  then
+    dateAlreadyThere=$(grep -c "date:" "$fname")
+    if [ "$dateAlreadyThere" == "0" ]
+    then
+      yfmAlreadyThere=$(grep -c "---" "$fname")
+      photoDate=$(git log --diff-filter=A --follow --format=%aI -- "$fname" | tail -1 | sed -e "s/T/ /" | sed -e "s/+/ +/")
+      echo "$fname"
+      echo " -> $photoDate"
+      if [ "$yfmAlreadyThere" == "0" ]
+      then
+        echo "---" >> "$fname"
+      fi
+      echo "date: $photoDate" >> "$fname"
+      if [ "$yfmAlreadyThere" == "0" ]
+      then
+        echo "---" >> "$fname"
+      fi
+    fi
+  fi
+done
