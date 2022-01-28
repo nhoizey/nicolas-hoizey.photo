6dbba29347286c698551190bb9aa272002d8d4bd
Nicolas Hoizey <nhoizey@clever-age.com>
2022-01-28 21:46:11 +0100

    Enhance script

--- _scripts/add-dates.sh
+++ _scripts/add-dates.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-find src/galleries -name "*.md" | while read fname; do
-  if [[ ! "$fname" =~ .*"index.md" ]]
+find src/galleries -name "*.md" | while read fileName; do
+  fileBasename=$(basename "$fileName")
+  if [ ! "$fileBasename" == "index.md" ]
   then
-    dateAlreadyThere=$(grep -c "date:" "$fname")
+    dateAlreadyThere=$(grep -c "date:" "$fileName")
     if [ "$dateAlreadyThere" == "0" ]
     then
-      yfmAlreadyThere=$(grep -c "\-\-\-" "$fname")
-      photoDate=$(git log --diff-filter=A --follow --format=%aI -- "$fname" | tail -1 | sed -e "s/T/ /" | sed -e "s/+/ +/")
-      echo "$fname"
+      yfmAlreadyThere=$(grep -c "\-\-\-" "$fileName")
+      # https://stackoverflow.com/a/32894198/717195
+      photoDate=$(git log --all --diff-filter=A --pretty=%x0a%ci --name-only | awk '
+/^$/        { dateline=!dateline; next }
+dateline    { date=$0; next }
+!seen[$0]++ { print date,$0 }
+' | grep "$fileBasename" | head -1)
+      photoDate="${photoDate:0:23}:00"
+      if [ "$yfmAlreadyThere" == "0" ]
+      then
+        echo "---" >> "$fileName"
+      fi
+      echo "date: $photoDate" >> "$fileName"
+      if [ "$yfmAlreadyThere" == "0" ]
+      then
+        echo "---" >> "$fileName"
+      fi
+      echo "$fileBasename"
       echo " -> $photoDate"
-      if [ "$yfmAlreadyThere" == "0" ]
-      then
-        echo "---" >> "$fname"
-      fi
-      echo "date: $photoDate" >> "$fname"
-      if [ "$yfmAlreadyThere" == "0" ]
-      then
-        echo "---" >> "$fname"
-      fi
+      echo ""
     fi
   fi
 done
