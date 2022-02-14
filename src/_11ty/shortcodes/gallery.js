17eff89b7f9bc281d97e0a4a70f2a0ebbfc314db
Nicolas Hoizey <nhoizey@clever-age.com>
2022-02-14 12:26:53 +0100

    Gallery shortcode

--- src/_11ty/shortcodes/gallery.js
+++ src/_11ty/shortcodes/gallery.js
@@ -1,57 +1,44 @@
-const fs = require('fs');
-const path = require('path');
-const matter = require('gray-matter');
 const moment = require('moment');
 
 module.exports = {
   gallery: (...slugs) => {
-    console.dir(globalUniquePhotos);
     if (slugs.length === 0) return '';
 
     let gallery = `<ul class="gallery">`;
     slugs.forEach((slug) => {
-      const md = fs.readFileSync(
-        path.join('./src/photos/', slug, 'index.md'),
-        'utf8'
+      const photosWithSlug = globalUniquePhotos.filter(
+        (page) => page.fileSlug === slug
       );
-      const content = matter(md);
-      gallery += `<li>
+      if (photosWithSlug.length === 1) {
+        const photo = photosWithSlug[0].data;
+        console.dir(photo);
+        const dimensions = photo.origin.data.dimensions;
+        gallery += `<li>
 <figure>
-  <img src="/photos/${slug}/${slug}.jpg" alt="${
-        content.data.title
-      }" class="frame${
-        content.data.dimensions.width < content.data.dimensions.height
-          ? ' portrait'
-          : ''
-      }" ${
-        content.data.dimensions.width
-          ? `width="${content.data.dimensions.width}`
-          : ''
-      } ${
-        content.data.dimensions.height
-          ? `height="${content.data.dimensions.height}`
-          : ''
-      } style="background-color: rgb(${
-        content.data.colors.muted
-      } / 50%)" data-responsiver="thumbnail_${
-        content.data.dimensions.width < content.data.dimensions.height
-          ? ' portrait'
-          : 'landscape'
-      }" />
+  <img src="/photos/${slug}/${slug}.jpg" alt="${photo.title}" class="frame${
+          dimensions.width < dimensions.height ? ' portrait' : ''
+        }" ${dimensions.width ? `width="${dimensions.width}"` : ''} ${
+          dimensions.height ? `height="${dimensions.height}"` : ''
+        } style="background-color: rgb(${
+          photo.origin.data.colors.muted
+        } / 50%)" data-responsiver="thumbnail_${
+          dimensions.width < dimensions.height ? ' portrait' : 'landscape'
+        }" />
   <figcaption>
-    <a href="{{ item.url }}">${content.data.title}</a>
+    <a href="${photo.page.url}">${photo.title}</a>
     <ul class="meta">
       <li class="date meta__item meta__date">
         <svg class="icon" aria-hidden="true"><use xlink:href="#date-icon" /></svg> <time class="dt-published" datetime="${moment(
-          content.data.date
+          photo.date
         ).format('YYYY-MM-DD')}">
-          ${moment(content.data.date).format('Do MMMM YYYY')}
+          ${moment(photo.date).format('Do MMMM YYYY')}
         </time>
       </li>
     </ul>
   </figcaption>
 </figure>
 </li>`;
+      }
     });
     gallery += '</ul>';
     return gallery;
