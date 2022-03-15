d5c432eda6bca7f07432e23a920cb4764e0a38e3
Nicolas Hoizey <nhoizey@clever-age.com>
2022-03-15 13:11:37 +0100

    Closer to native fetch

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,27 +1,36 @@
-const axios = require('axios');
-
-const CLOUDFLARE_API_URL = `https://api.cloudflare.com/client/v4/zones/${process.env.CLOUDFLARE_ZONE_ID}/purge_cache`;
-const CLOUDFLARE_API_HEADERS = {
-  'X-Auth-Email': `${process.env.CLOUDFLARE_EMAIL}`,
-  Authorization: `Bearer ${process.env.CLOUDFLARE_PURGE_KEY}`,
-  'Content-Type': 'application/json',
-};
+import fetch from 'node-fetch';
 
 exports.handler = async () => {
-  axios
-    .post(CLOUDFLARE_API_URL, '{ "purge_everything": true }', {
-      headers: CLOUDFLARE_API_HEADERS,
-    })
+  fetch(
+    `https://api.cloudflare.com/client/v4/zones/${process.env.CLOUDFLARE_ZONE_ID}/purge_cache`,
+    {
+      method: 'POST',
+      body: JSON.stringify({ purge_everything: true }),
+      headers: {
+        'X-Auth-Email': `${process.env.CLOUDFLARE_EMAIL}`,
+        Authorization: `Bearer ${process.env.CLOUDFLARE_PURGE_KEY}`,
+        'Content-Type': 'application/json',
+      },
+    }
+  )
     .then((res) => {
-      return {
-        statusCode: 200,
-        body: JSON.stringify(res.data),
-      };
+      if (res.ok) {
+        return res;
+      } else {
+        throw new Error(
+          `The HTTP status of the reponse: ${res.status} (${res.statusText})`
+        );
+      }
     })
+    .then((res) => res.json())
+    .then((json) => ({
+      statusCode: 200,
+      body: JSON.stringify(json),
+    }))
     .catch((err) => {
       return {
         statusCode: 500,
-        body: JSON.stringify(err.message),
+        body: JSON.stringify(err),
       };
     });
 };
