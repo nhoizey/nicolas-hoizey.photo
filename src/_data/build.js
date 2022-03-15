043e4124cb048b6d526c0e7d7756c74dbb818c17
Nicolas Hoizey <nhoizey@clever-age.com>
2022-03-15 09:11:51 +0100

    Back on Netlify

--- src/_data/build.js
+++ src/_data/build.js
@@ -2,7 +2,6 @@ const timestamp = new Date();
 
 module.exports = {
   env: process.env.NODE_ENV,
-  service: process.env.SERVICE || 'Cloudflare',
   timestamp: timestamp,
   id: timestamp.valueOf(),
 };
