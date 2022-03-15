f57ba94b11bad0cdda6a2da85afaf7a9e1f08299
Nicolas Hoizey <nhoizey@clever-age.com>
2022-03-15 12:40:20 +0100

    Refactor function

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,21 +1,16 @@
 const axios = require('axios');
 
+const CLOUDFLARE_API_URL = `https://api.cloudflare.com/client/v4/zones/${process.env.CLOUDFLARE_ZONE_ID}/purge_cache`;
+const CLOUDFLARE_API_HEADERS = {
+  'X-Auth-Email': `${process.env.CLOUDFLARE_EMAIL}`,
+  Authorization: `Bearer ${process.env.CLOUDFLARE_PURGE_KEY}`,
+  'Content-Type': 'application/json',
+};
+
 exports.handler = async () => {
-  const headers = {
-    'X-Auth-Email': `${process.env.CLOUDFLARE_EMAIL}`,
-    Authorization: `Bearer ${process.env.CLOUDFLARE_PURGE_KEY}`,
-    'Content-Type': 'application/json',
-  };
-
-  const callbackHeaders = {
-    'Content-Type': 'application/vnd.api+json; charset=utf=8',
-  };
-
-  const CLOUDFLARE_API = `https://api.cloudflare.com/client/v4/zones/${process.env.CLOUDFLARE_ZONE_ID}/purge_cache`;
-
   axios
-    .post(CLOUDFLARE_API, '{ "purge_everything": true }', {
-      headers: headers,
+    .post(CLOUDFLARE_API_URL, '{ "purge_everything": true }', {
+      headers: CLOUDFLARE_API_HEADERS,
     })
     .then((res) => {
       return {
