d0aa353c754ac92ea199b7ea6f2723b0089c015c
Nicolas Hoizey <nhoizey@clever-age.com>
2022-03-19 00:41:10 +0100

    no cross origin

--- src/_11ty/images-responsiver-config.js
+++ src/_11ty/images-responsiver-config.js
@@ -89,9 +89,9 @@ module.exports = {
     minWidth: 360,
     maxWidth: 1600,
     sizes: '(max-width: 67rem) 90vw, 60rem',
-    attributes: {
-      crossorigin: 'anonymous',
-    },
+    // attributes: {
+    //   crossorigin: 'anonymous',
+    // },
   },
   landscape: {
     maxWidth: 3500,
