2a5dd0f4c2cb0805b5fa482a3ad1e3d2d0fec94d
Nicolas Hoizey <nhoizey@clever-age.com>
2022-06-07 23:32:29 +0200

    Enhance opengraph images creation

--- _scripts/generate-opengraph-images.js
+++ _scripts/generate-opengraph-images.js
@@ -3,52 +3,65 @@
 require('dotenv').config();
 
 const puppeteer = require('puppeteer-core');
-const fs = require('fs');
+const { Cluster } = require('puppeteer-cluster');
+const fs = require('fs').promises;
 const path = require('path');
 
-const SRC = './src/photos/';
-
-async function syncOnePhoto(slug) {
-  if (slug === '.DS_Store') return;
-
-  const opengraphFile = path.join(SRC, slug, 'opengraph.jpg');
-  if (fs.existsSync(opengraphFile)) return;
-
-  console.log(`Get opengraph image for ${slug}`);
-
-  const opengraphHtmlUrl = `https://nicolas-hoizey.photo/photos/${slug}/opengraph.html`;
-  const browser = await puppeteer.launch({
-    executablePath:
-      '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+(async () => {
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_PAGE,
+    maxConcurrency: 5,
+    monitor: true,
+    puppeteer: puppeteer,
+    puppeteerOptions: {
+      executablePath:
+        '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    },
   });
-  const page = await browser.newPage();
-  page.setViewport({
-    width: 1440,
-    height: 800,
-    deviceScaleFactor: 1,
-  });
-  await page.goto(opengraphHtmlUrl, { waitUntil: 'load', timeout: 0 });
 
-  // Take a screenshot of the opengraph image
-  const opengraphElement = await page.$('#opengraph');
-  if (opengraphElement) {
-    const opengraphScreenshotResult = await opengraphElement.screenshot({
-      path: opengraphFile,
-      type: 'jpeg',
+  await cluster.task(async ({ page, data: resourcePath }) => {
+    const folder = path.join('./src', resourcePath);
+    const isDir = await fs.stat(folder).then((stats) => stats.isDirectory());
+    if (!isDir) {
+      return;
+    }
+
+    const file = path.join(folder, 'opengraph.jpg');
+    const fileExists = await fs
+      .access(file)
+      .then(() => true)
+      .catch(() => false);
+    if (fileExists) return;
+
+    const opengraphUrl = `https://nicolas-hoizey.photo/${resourcePath}/opengraph.html`;
+
+    console.log(`Get opengraph image for ${opengraphUrl}`);
+
+    await page.setViewport({
+      width: 1440,
+      height: 800,
+      deviceScaleFactor: 1,
     });
-  }
+    await page.goto(opengraphUrl, { waitUntil: 'networkidle0', timeout: 0 });
 
-  await page.close();
-  await browser.close();
-}
+    // Save a screenshot of the opengraph image
+    const element = await page.$('#opengraph');
+    if (element) {
+      await element.screenshot({
+        path: file,
+        type: 'jpeg',
+      });
+    }
+  });
 
-async function syncAllPhotos() {
-  await fs.readdirSync(SRC).reduce(async (accumulator, photo) => {
-    return [...(await accumulator), await syncOnePhoto(photo)];
-  }, Promise.resolve([]));
-}
+  // In case of problems, log them
+  cluster.on('taskerror', (err, data) => {
+    console.log(`  Error with ${data}: ${err.message}`);
+  });
 
-syncAllPhotos().then(() => {
-  // Todo after everything else
-  console.log('Done!');
-});
+  const photos = await fs.readdir('./src/photos/');
+  photos.forEach((photo) => cluster.queue(path.join('photos', photo)));
+
+  await cluster.idle();
+  await cluster.close();
+})();
