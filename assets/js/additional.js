fd236de38530141de2da4f434154b5b16116ad06
Nicolas Hoizey <nhoizey@clever-age.com>
2022-06-12 22:11:15 +0200

    Deal with lazy loaded responsive images

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -1,6 +1,10 @@
 // Load galleries images if motion is accepted
 if (window.matchMedia('(prefers-reduced-motion: no-preference)').matches) {
   document.querySelectorAll('.diaporama').forEach((element) => {
+    element.querySelectorAll('img[data-srcset]').forEach((image) => {
+      image.setAttribute('srcset', image.dataset.srcset);
+      delete image.dataset.srcset;
+    });
     element.querySelectorAll('img[data-src]').forEach((image) => {
       image.setAttribute('src', image.dataset.src);
       delete image.dataset.src;
