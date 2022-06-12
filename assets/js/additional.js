ba26fa6501474f599969be011e063dab6eb41f7b
Nicolas Hoizey <nhoizey@clever-age.com>
2022-06-12 22:03:54 +0200

    Load images 2, 3 and 4 of diaporamas only if animations
    Fixes #130

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -1,1 +1,10 @@
+// Load galleries images if motion is accepted
+if (window.matchMedia('(prefers-reduced-motion: no-preference)').matches) {
+  document.querySelectorAll('.diaporama').forEach((element) => {
+    element.querySelectorAll('img[data-src]').forEach((image) => {
+      image.setAttribute('src', image.dataset.src);
+      delete image.dataset.src;
+    });
+    element.classList.add('loaded');
+  });
 }
