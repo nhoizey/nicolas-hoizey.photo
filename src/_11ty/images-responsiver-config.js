9a6a00e9d893389f6f89702f1a28af74ca59bdef
Nicolas Hoizey <nhoizey@clever-age.com>
2022-06-14 16:53:59 +0200

    Images in Ken Burns are larger

--- src/_11ty/images-responsiver-config.js
+++ src/_11ty/images-responsiver-config.js
@@ -119,11 +119,11 @@ module.exports = {
   diaporama: {
     resizedImageUrl: (src, width) =>
       `https://res.cloudinary.com/nho/image/fetch/q_auto,f_auto,g_auto,w_${width},ar_3:2,c_fill/${src}`,
-    fallbackWidth: 384,
-    minWidth: 384, // 320 * 1.2
-    maxWidth: 768, // No need for more than 2dppx
-    steps: 3,
-    sizes: '(min-width: 22rem) calc(1.2 * 20rem), calc(1.2 * (100vw - 2rem))',
+    fallbackWidth: 416,
+    minWidth: 416, // 320 * 1.3
+    maxWidth: 832, // No need for more than 2dppx
+    steps: 4,
+    sizes: '(min-width: 22rem) calc(1.3 * 20rem), calc(1.3 * (100vw - 2rem))',
     figure: 'never',
   },
   thumbnail: {
@@ -132,7 +132,7 @@ module.exports = {
     fallbackWidth: 320,
     minWidth: 320,
     maxWidth: 640, // No need for more than 2dppx
-    steps: 3,
+    steps: 4,
     sizes: '(min-width: 22rem) 20rem, calc(100vw - 2rem)',
     figure: 'never',
   },
