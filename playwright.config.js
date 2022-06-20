963388a50a90e095db38db271d7c8fb82af361b8
Nicolas Hoizey <nhoizey@clever-age.com>
2022-06-20 23:38:36 +0200

    Add tests with Playwright

---
+++ playwright.config.js
@@ -0,0 +1,37 @@
+const { devices } = require('@playwright/test');
+
+const config = {
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  use: {
+    baseURL: 'http://localhost:8080',
+    // headless: false,
+    viewport: { width: 1280, height: 720 },
+    javaScriptEnabled: false,
+    trace: 'on-first-retry',
+    video: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      channel: 'chrome',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'iPhone 11',
+      use: {
+        browserName: 'webkit',
+        ...devices['iPhone 11'],
+      },
+    },
+  ],
+  expect: {
+    toHaveScreenshot: { maxDiffPixels: 1000 },
+  },
+};
+
+module.exports = config;
