4ea982079b961cd72fe88698f1875815b7545eb5
Nicolas Hoizey <nhoizey@clever-age.com>
2022-07-20 12:21:40 +0200

    Debug postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require('autoprefixer'), require('cssnano')],
+  plugins: [
+    require('autoprefixer'),
+    require('cssnano'),
+    require('postcss-fail-on-warn'),
+  ],
 };
