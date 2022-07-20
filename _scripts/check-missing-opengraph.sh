dc180e4c33fe67b442b4970eabe914f5a14825ff
Nicolas Hoizey <nhoizey@clever-age.com>
2022-07-20 09:41:40 +0200

    Enhance pre-push hook

---
+++ _scripts/check-missing-opengraph.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+missing_opengraph=$(find src/photos/ -mindepth 1 -type d '!' -exec test -e "{}/opengraph.jpg" ';' -print | wc -l)
+missing_opengraph="$((missing_opengraph + 0))"
+if [ ! "$missing_opengraph" -eq "0" ]
+then
+  echo "Run ./_scripts/generate-opengraph-images.js to generate missing opengraph.jpg files"
+  exit 1
+fi
+exit 0
