7e2263ec5538e16f5cd641cbd40188892e9629a5
Nicolas Hoizey <nhoizey@clever-age.com>
2022-07-20 12:42:32 +0200

    Add postcss-logical for old browsers support

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    require('postcss-logical')({ dir: 'ltr', preserve: true }),
     require('autoprefixer'),
     require('cssnano'),
     require('postcss-fail-on-warn'),
