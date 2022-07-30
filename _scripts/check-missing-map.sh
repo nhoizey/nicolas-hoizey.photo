c9d3ddb31a5a7bff029778dff30dd241a2d88e88
Nicolas Hoizey <nhoizey@clever-age.com>
2022-07-30 23:04:51 +0200

    Finalize map generation

---
+++ _scripts/check-missing-map.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+missing_map=$(find src/photos/ -mindepth 1 -type d '!' -exec test -e "{}/map.png" ';' -print | wc -l)
+missing_map="$((missing_map + 0))"
+if [ ! "$missing_map" -eq "0" ]
+then
+  echo "Run ./_scripts/generate-map-images.js to generate missing map.png files"
+  exit 1
+fi
+exit 0
