61b814eb05980fe0826b1cf7823fe09587f69a05
Nicolas Hoizey <nhoizey@clever-age.com>
2022-10-13 17:34:30 +0200

    Enhance Matomo loading performance
    Follow https://csswizardry.com/2022/10/speeding-up-async-snippets/#the-new-syntax

--- assets/js/critical.js
+++ assets/js/critical.js
@@ -13,26 +13,18 @@
   );
 
   if (process.env.NODE_ENV === 'production') {
-    // Matomo
+    // Prepare Matomo data
     var _paq = (window._paq = window._paq || []);
+
     /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
     _paq.push(['setCustomDimension', 3, window.screen_density]);
     _paq.push(['setCustomDimension', 4, window.viewport_width]);
 
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
-    (function () {
-      var u = 'https://data.nicolas-hoizey.com/';
-      _paq.push(['setTrackerUrl', u + 'matomo.php']);
-      _paq.push(['setSiteId', '11']);
-      var d = document,
-        g = d.createElement('script'),
-        s = d.getElementsByTagName('script')[0];
-      g.type = 'text/javascript';
-      g.async = true;
-      g.src = '/ui/js/matomo.js';
-      g.crossorigin = 'anonymous';
-      s.parentNode.insertBefore(g, s);
-    })();
+
+    var u = 'https://data.nicolas-hoizey.com/';
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    _paq.push(['setSiteId', '11']);
   }
 })(window);
