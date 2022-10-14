72d0176d87c17be9288ee50b528ca55487be740b
Nicolas Hoizey <nhoizey@clever-age.com>
2022-10-14 16:29:52 +0200

    Wait 1 second before running diaporama

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -1,14 +1,22 @@
-// Load galleries images if motion is accepted
-if (window.matchMedia('(prefers-reduced-motion: no-preference)').matches) {
-  document.querySelectorAll('.diaporama').forEach((element) => {
-    element.querySelectorAll('img[data-srcset]').forEach((image) => {
-      image.setAttribute('srcset', image.dataset.srcset);
-      delete image.dataset.srcset;
+const runDiaporama = async () => {
+  // Load galleries images if motion is accepted
+  if (window.matchMedia('(prefers-reduced-motion: no-preference)').matches) {
+    // wait for 1 second
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Load all images
+    document.querySelectorAll('.diaporama').forEach((element) => {
+      element.querySelectorAll('img[data-srcset]').forEach((image) => {
+        image.setAttribute('srcset', image.dataset.srcset);
+        delete image.dataset.srcset;
+      });
+      element.querySelectorAll('img[data-src]').forEach((image) => {
+        image.setAttribute('src', image.dataset.src);
+        delete image.dataset.src;
+      });
+      element.classList.add('loaded');
     });
-    element.querySelectorAll('img[data-src]').forEach((image) => {
-      image.setAttribute('src', image.dataset.src);
-      delete image.dataset.src;
-    });
-    element.classList.add('loaded');
-  });
-}
+  }
+};
+
+runDiaporama();
