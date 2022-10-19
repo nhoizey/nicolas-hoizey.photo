2e1c6ca296d8e1a617e3e68ac100661db9c89171
Nicolas Hoizey <nhoizey@clever-age.com>
2022-10-19 12:31:43 +0200

    Pause diaporamas when window is hidden

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -1,14 +1,17 @@
-const runDiaporama = async () => {
-  // Load galleries images if motion is accepted
-  if (window.matchMedia('(prefers-reduced-motion: no-preference)').matches) {
+const isDiaporamaLoaded = false;
+const initialPageTitle = document.title;
+
+const loadDiaporama = async () => {
+  // Load diaporama images if motion is accepted
+  if (
+    !isDiaporamaLoaded &&
+    window.matchMedia('(prefers-reduced-motion: no-preference)').matches
+  ) {
     // wait for 1 second
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
     // Load all images
     document.querySelectorAll('.diaporama').forEach((element) => {
-      // Run animations
-      element.classList.add('run');
-
       // Load and animate lazy images
       element.querySelectorAll('img[data-srcset]').forEach((image) => {
         image.setAttribute('srcset', image.dataset.srcset);
@@ -19,7 +22,43 @@ const runDiaporama = async () => {
         delete image.dataset.src;
       });
     });
+
+    isDiaporamaLoaded = true;
   }
 };
 
-runDiaporama();
+const runDiaporama = () => {
+  loadDiaporama();
+  document.querySelector('body').classList.add('run_diaporamas');
+  document.title = initialPageTitle;
+};
+
+const stopDiaporama = () => {
+  document.querySelector('body').classList.remove('run_diaporamas');
+  document.title = `⏸️ ${initialPageTitle}`;
+};
+
+const toggleDiaporamaRunningState = () => {
+  if (document.hidden) {
+    stopDiaporama();
+  } else {
+    runDiaporama();
+  }
+};
+
+if (typeof document.hidden === 'undefined') {
+  // No support for the Page Visibility API
+  runDiaporama();
+} else {
+  // Run the diaporamas only if the page is visible
+  if (!document.hidden) {
+    runDiaporama();
+  }
+
+  // Add an event listener for page visibility change
+  document.addEventListener(
+    'visibilitychange',
+    toggleDiaporamaRunningState,
+    false
+  );
+}
