3f9d1fdc8c83cb800d150a6b9efb8b2b17ca4a47
Nicolas Hoizey <nicolas@hoizey.com>
2022-12-03 02:05:17 +0100

    New JSON feed for POSSE

--- src/_11ty/filters/string.js
+++ src/_11ty/filters/string.js
@@ -7,4 +7,23 @@ module.exports = {
   },
   clean4cloudinary: (string) =>
     string.replace(',', '%252C').replace('/', '%252F').replace('?', '%253F'),
+  tagToHashtag: (tag) => {
+    let hashtag = tag.toString();
+    hashtag = hashtag.replaceAll('/', ' ');
+    // Use https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+    hashtag = hashtag.normalize('NFD');
+    // Remove https://en.wikipedia.org/wiki/Combining_Diacritical_Marks
+    hashtag = hashtag.replace(/[\u0300-\u036f]/g, '');
+
+    hashtag = hashtag.replaceAll('&#39;', ' ');
+
+    let words = hashtag.replaceAll(/[-\.]/g, ' ').split(' ');
+    return (
+      words[0] +
+      words
+        .slice(1)
+        .map((word) => word.charAt(0).toUpperCase() + word.substr(1))
+        .join('')
+    );
+  },
 };
