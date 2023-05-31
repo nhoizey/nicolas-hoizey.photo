ac96d9d3ad4957aeb4ed5398cce65ddf8c43986f
Nicolas Hoizey <nicolas@hoizey.com>
2023-05-31 15:54:49 +0200

    Remove Matomo

--- assets/js/critical.js
+++ assets/js/critical.js
@@ -7,30 +7,4 @@
 
   // Style for Mapbox
   window.mapboxStyle = 'mapbox/outdoors-v11';
-
-  // get device pixel ratio in dppx
-  window.screen_density = window.devicePixelRatio || 0;
-
-  // get viewport width
-  // http://stackoverflow.com/a/8876069/717195
-  window.viewport_width = Math.max(
-    window.document.documentElement.clientWidth,
-    window.innerWidth || 0
-  );
-
-  if (process.env.NODE_ENV === 'production') {
-    // Prepare Matomo data
-    var _paq = (window._paq = window._paq || []);
-
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(['setCustomDimension', 3, window.screen_density]);
-    _paq.push(['setCustomDimension', 4, window.viewport_width]);
-
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-
-    var u = 'https://data.nicolas-hoizey.com/';
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', '11']);
-  }
 })(window);
