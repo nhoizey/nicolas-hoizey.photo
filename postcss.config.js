4e758c36352c7dc67e6c3cc34de0af02ce62b856
Nicolas Hoizey <nicolas@hoizey.com>
2023-06-09 19:20:00 +0200

    Autoprefixer doesn't like transition properties
    https://github.com/postcss/autoprefixer/issues/896#issuecomment-1107520793

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-logical')({ dir: 'ltr' }),
+    require('postcss-ignore-transition-properties'),
     require('autoprefixer'),
     require('postcss-fail-on-warn'),
   ],
