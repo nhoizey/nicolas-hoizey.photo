cee7e150b88e24372d73c618dc4707d3558aa86a
Nicolas Hoizey <nicolas@hoizey.com>
2023-06-15 14:57:52 +0200

    Webmentions

--- src/_11ty/filters/dates.js
+++ src/_11ty/filters/dates.js
@@ -17,8 +17,13 @@ const dateObj = (eleventyDate) => {
 module.exports = {
   // 14 October 1983
   readableDate: (date) => dateObj(date).toLocaleString(DateTime.DATE_FULL),
+  // 14 October 1983
+  readableDateTime: (date) =>
+    dateObj(date).toLocaleString(DateTime.DATETIME_FULL),
   // 1983-10-14
   attributeDate: (date) => dateObj(date).toISODate(),
   // 1983-10-14T20:04:00.000Z
   isoDate: (date) => dateObj(date).toUTC().toISO(),
+  readableDateFromIso: (date) =>
+    DateTime.fromISO(date).toLocaleString(DateTime.DATETIME_FULL),
 };
