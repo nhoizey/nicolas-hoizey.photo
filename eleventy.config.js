58cfada43af6e48c9d92cb016aea7bf389cbfee2
Nicolas Hoizey <nicolas@hoizey.com>
2024-04-10 11:29:10 +0200

    Replace spaces with tabs

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,226 +6,230 @@ const glob = require('fast-glob');
 const path = require('path');
 
 module.exports = function (eleventyConfig) {
-  // ------------------------------------------------------------------------
-  // Collections
-  // ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	// Collections
+	// ------------------------------------------------------------------------
 
-  // Initialize global variable to store a list of all photos
-  // Will be filed during computing of collections with getUniquePhotos() function
-  let globalUniquePhotos = [];
+	// Initialize global variable to store a list of all photos
+	// Will be filed during computing of collections with getUniquePhotos() function
+	let globalUniquePhotos = [];
 
-  glob.sync('src/_11ty/collections/*.js').forEach((file) => {
-    let collectionList = require('./' + file);
-    Object.keys(collectionList).forEach((name) => {
-      eleventyConfig.addCollection(name, collectionList[name]);
-    });
-  });
+	glob.sync('src/_11ty/collections/*.js').forEach((file) => {
+		let collectionList = require('./' + file);
+		Object.keys(collectionList).forEach((name) => {
+			eleventyConfig.addCollection(name, collectionList[name]);
+		});
+	});
 
-  // ------------------------------------------------------------------------
-  // Filters
-  // ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	// Filters
+	// ------------------------------------------------------------------------
 
-  glob.sync('src/_11ty/filters/*.js').forEach((file) => {
-    let filters = require('./' + file);
-    Object.keys(filters).forEach((name) => {
-      eleventyConfig.addFilter(name, filters[name]);
-    });
-  });
+	glob.sync('src/_11ty/filters/*.js').forEach((file) => {
+		let filters = require('./' + file);
+		Object.keys(filters).forEach((name) => {
+			eleventyConfig.addFilter(name, filters[name]);
+		});
+	});
 
-  // ------------------------------------------------------------------------
-  // Shortcodes
-  // ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	// Shortcodes
+	// ------------------------------------------------------------------------
 
-  glob.sync('src/_11ty/shortcodes/*.js').forEach((file) => {
-    let shortcodes = require('./' + file);
-    Object.keys(shortcodes).forEach((name) => {
-      eleventyConfig.addNunjucksShortcode(name, shortcodes[name]);
-    });
-  });
+	glob.sync('src/_11ty/shortcodes/*.js').forEach((file) => {
+		let shortcodes = require('./' + file);
+		Object.keys(shortcodes).forEach((name) => {
+			eleventyConfig.addNunjucksShortcode(name, shortcodes[name]);
+		});
+	});
 
-  // ------------------------------------------------------------------------
-  // Plugins
-  // ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	// Plugins
+	// ------------------------------------------------------------------------
 
-  eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-rss'));
+	eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-rss'));
 
-  eleventyConfig.addPlugin(require('eleventy-plugin-embed-everything'), {
-    youtube: {
-      options: {
-        lite: {
-          css: {
-            path: '/ui/js/yt-lite/lite-yt-embed.css',
-          },
-          js: {
-            path: '/ui/js/yt-lite/lite-yt-embed.js',
-          },
-        },
-      },
-    },
-  });
+	eleventyConfig.addPlugin(require('eleventy-plugin-embed-everything'), {
+		youtube: {
+			options: {
+				lite: {
+					css: {
+						path: '/ui/js/yt-lite/lite-yt-embed.css',
+					},
+					js: {
+						path: '/ui/js/yt-lite/lite-yt-embed.js',
+					},
+				},
+			},
+		},
+	});
 
-  // ------------------------------------------------------------------------
-  // Transforms
-  // ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	// Transforms
+	// ------------------------------------------------------------------------
 
-  if (process.env.NODE_ENV === 'production') {
-    eleventyConfig.addPlugin(
-      require('eleventy-plugin-images-responsiver'),
-      require(path.join(__dirname, 'src/_11ty/images-responsiver-config.js'))
-    );
+	if (process.env.NODE_ENV === 'production') {
+		eleventyConfig.addPlugin(
+			require('eleventy-plugin-images-responsiver'),
+			require(path.join(__dirname, 'src/_11ty/images-responsiver-config.js'))
+		);
 
-    // eleventyConfig.addTransform(
-    //   'htmlmin',
-    //   require(path.join(
-    //     __dirname,
-    //     'src/_11ty/transforms/html-min-transform.js'
-    //   ))
-    // );
+		// eleventyConfig.addTransform(
+		//   'htmlmin',
+		//   require(path.join(
+		//     __dirname,
+		//     'src/_11ty/transforms/html-min-transform.js'
+		//   ))
+		// );
 
-    // eleventyConfig.addPlugin(require('eleventy-plugin-auto-preload'));
-  }
+		// eleventyConfig.addPlugin(require('eleventy-plugin-auto-preload'));
+	}
 
-  // ------------------------------------------------------------------------
-  // Markdown plugins
-  // ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	// Markdown plugins
+	// ------------------------------------------------------------------------
 
-  const markdownIt = require('markdown-it');
-  const markdownItOptions = {
-    html: true,
-    breaks: true,
-    linkify: true,
-  };
+	const markdownIt = require('markdown-it');
+	const markdownItOptions = {
+		html: true,
+		breaks: true,
+		linkify: true,
+	};
 
-  const markdownItFootnote = require('markdown-it-footnote');
+	const markdownItFootnote = require('markdown-it-footnote');
 
-  const slugify = require(path.join(__dirname, 'src/_11ty/_utils/slugify.js'));
-  const markdownItAnchor = require('markdown-it-anchor');
-  // https://www.toptal.com/designers/htmlarrows/punctuation/section-sign/
-  const markdownItAnchorOptions = {
-    permalink: true,
-    permalinkClass: 'deeplink',
-    permalinkSymbol: '&#xa7;&#xFE0E;',
-    level: [2, 3, 4],
-    slugify: function (s) {
-      return slugify(s);
-    },
-  };
+	const slugify = require(path.join(__dirname, 'src/_11ty/_utils/slugify.js'));
+	const markdownItAnchor = require('markdown-it-anchor');
+	// https://www.toptal.com/designers/htmlarrows/punctuation/section-sign/
+	const markdownItAnchorOptions = {
+		permalink: true,
+		permalinkClass: 'deeplink',
+		permalinkSymbol: '&#xa7;&#xFE0E;',
+		level: [2, 3, 4],
+		slugify: function (s) {
+			return slugify(s);
+		},
+	};
 
-  const markdownItAttributes = require('markdown-it-attrs');
+	const markdownItAttributes = require('markdown-it-attrs');
 
-  const markdownItSpan = require('markdown-it-bracketed-spans');
+	const markdownItSpan = require('markdown-it-bracketed-spans');
 
-  const markdownItContainer = require('markdown-it-container');
+	const markdownItContainer = require('markdown-it-container');
 
-  const markdownItAbbr = require('markdown-it-abbr');
+	const markdownItAbbr = require('markdown-it-abbr');
 
-  // taken from https://gist.github.com/rodneyrehm/4feec9af8a8635f7de7cb1754f146a39
-  function getHeadingLevel(tagName) {
-    if (tagName[0].toLowerCase() === 'h') {
-      tagName = tagName.slice(1);
-    }
+	// taken from https://gist.github.com/rodneyrehm/4feec9af8a8635f7de7cb1754f146a39
+	function getHeadingLevel(tagName) {
+		if (tagName[0].toLowerCase() === 'h') {
+			tagName = tagName.slice(1);
+		}
 
-    return parseInt(tagName, 10);
-  }
+		return parseInt(tagName, 10);
+	}
 
-  function markdownItHeadingLevel(md, options) {
-    var firstLevel = options.firstLevel;
+	function markdownItHeadingLevel(md, options) {
+		var firstLevel = options.firstLevel;
 
-    if (typeof firstLevel === 'string') {
-      firstLevel = getHeadingLevel(firstLevel);
-    }
+		if (typeof firstLevel === 'string') {
+			firstLevel = getHeadingLevel(firstLevel);
+		}
 
-    if (!firstLevel || isNaN(firstLevel)) {
-      return;
-    }
+		if (!firstLevel || isNaN(firstLevel)) {
+			return;
+		}
 
-    var levelOffset = firstLevel - 1;
-    if (levelOffset < 1 || levelOffset > 6) {
-      return;
-    }
+		var levelOffset = firstLevel - 1;
+		if (levelOffset < 1 || levelOffset > 6) {
+			return;
+		}
 
-    md.core.ruler.push('adjust-heading-levels', function (state) {
-      var tokens = state.tokens;
-      for (var i = 0; i < tokens.length; i++) {
-        if (tokens[i].type !== 'heading_close') {
-          continue;
-        }
+		md.core.ruler.push('adjust-heading-levels', function (state) {
+			var tokens = state.tokens;
+			for (var i = 0; i < tokens.length; i++) {
+				if (tokens[i].type !== 'heading_close') {
+					continue;
+				}
 
-        var headingOpen = tokens[i - 2];
-        var headingClose = tokens[i];
+				var headingOpen = tokens[i - 2];
+				var headingClose = tokens[i];
 
-        var currentLevel = getHeadingLevel(headingOpen.tag);
-        var tagName = 'h' + Math.min(currentLevel + levelOffset, 6);
+				var currentLevel = getHeadingLevel(headingOpen.tag);
+				var tagName = 'h' + Math.min(currentLevel + levelOffset, 6);
 
-        headingOpen.tag = tagName;
-        headingClose.tag = tagName;
-      }
-    });
-  }
+				headingOpen.tag = tagName;
+				headingClose.tag = tagName;
+			}
+		});
+	}
 
-  const md = markdownIt(markdownItOptions)
-    .use(markdownItHeadingLevel, { firstLevel: 2 })
-    .use(markdownItFootnote)
-    .use(markdownItAnchor, markdownItAnchorOptions)
-    .use(markdownItAttributes)
-    .use(markdownItSpan)
-    .use(markdownItAbbr)
-    .use(markdownItContainer, 'info')
-    .use(markdownItContainer, 'success')
-    .use(markdownItContainer, 'warning')
-    .use(markdownItContainer, 'error');
-  eleventyConfig.setLibrary('md', md);
+	const md = markdownIt(markdownItOptions)
+		.use(markdownItHeadingLevel, { firstLevel: 2 })
+		.use(markdownItFootnote)
+		.use(markdownItAnchor, markdownItAnchorOptions)
+		.use(markdownItAttributes)
+		.use(markdownItSpan)
+		.use(markdownItAbbr)
+		.use(markdownItContainer, 'info')
+		.use(markdownItContainer, 'success')
+		.use(markdownItContainer, 'warning')
+		.use(markdownItContainer, 'error');
+	eleventyConfig.setLibrary('md', md);
 
-  // Add markdownify filter with Markdown-it configuration
-  eleventyConfig.addFilter('markdownify', (markdownString) =>
-    md.render(markdownString)
-  );
+	// Add markdownify filter with Markdown-it configuration
+	eleventyConfig.addFilter('markdownify', (markdownString) =>
+		md.render(markdownString)
+	);
 
-  // Add markdown paired shortcode with shared Markdown-it configuration
-  eleventyConfig.addPairedShortcode(
-    'markdown',
-    (markdownString, inline = null) =>
-      inline ? md.renderInline(markdownString) : md.render(markdownString)
-  );
+	// Add markdown paired shortcode with shared Markdown-it configuration
+	eleventyConfig.addPairedShortcode(
+		'markdown',
+		(markdownString, inline = null) =>
+			inline ? md.renderInline(markdownString) : md.render(markdownString)
+	);
 
-  // ------------------------------------------------------------------------
-  // Eleventy configuration
-  // ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	// Eleventy configuration
+	// ------------------------------------------------------------------------
 
-  eleventyConfig
-    .addPassthroughCopy(
-      'src/{assets,photos,galleries,blog}/**/*.{jpg,jpeg,png,gif}'
-    )
-    .addPassthroughCopy('src/ui')
-    .addPassthroughCopy('src/robots.txt')
-    .addPassthroughCopy('src/favicon.ico');
+	eleventyConfig
+		.addPassthroughCopy(
+			'src/{assets,photos,galleries,blog}/**/*.{jpg,jpeg,png,gif}'
+		)
+		.addPassthroughCopy('src/ui')
+		.addPassthroughCopy('src/robots.txt')
+		.addPassthroughCopy('src/favicon.ico');
 
-  eleventyConfig.setUseGitIgnore(false);
-  eleventyConfig.addWatchTarget('./ui/');
+	eleventyConfig.setUseGitIgnore(false);
+	eleventyConfig.addWatchTarget('./ui/');
 
-  eleventyConfig.setDataDeepMerge(true);
-  eleventyConfig.setQuietMode(true);
+	eleventyConfig.setDataDeepMerge(true);
+	eleventyConfig.setQuietMode(true);
 
-  eleventyConfig.setServerOptions({
-    watch: ['_site/ui/css/**/*.css', '_site/ui/js/**/*.js'],
-  });
+	eleventyConfig.setServerOptions({
+		watch: ['_site/ui/css/**/*.css', '_site/ui/js/**/*.js'],
+	});
 
-  // if (process.env.NODE_ENV === 'production') {
-  //   eleventyConfig.ignores.add('src/tools/opengraph');
-  // }
+	// if (process.env.NODE_ENV === 'production') {
+	//   eleventyConfig.ignores.add('src/tools/opengraph');
+	// }
 
-  return {
-    templateFormats: ['md', 'njk'],
+	// eleventyConfig.on('eleventy.contentMap', ({ inputPathToUrl }) => {
+	// 	console.log(inputPathToUrl);
+	// });
 
-    markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-    dir: {
-      output: '_site',
-      input: 'src',
-      includes: '_includes',
-      layouts: '_layouts',
-      data: '_data',
-    },
-  };
+	return {
+		templateFormats: ['md', 'njk'],
+
+		markdownTemplateEngine: 'njk',
+		htmlTemplateEngine: 'njk',
+		dataTemplateEngine: 'njk',
+		dir: {
+			output: '_site',
+			input: 'src',
+			includes: '_includes',
+			layouts: '_layouts',
+			data: '_data',
+		},
+	};
 };
