824596370a769dcc644266f2c532833cfa1a4dc5
Nicolas Hoizey <nicolas@hoizey.com>
2024-07-19 01:47:14 +0200

    Restore some collections

--- src/_11ty/collections/navigation.js
+++ src/_11ty/collections/navigation.js
@@ -1,15 +1,15 @@
-// // Add a "navigation" collection with all auto collection homepages
+// Add a "navigation" collection with all auto collection homepages
 
-// const sortOrderThenAlpha = require('../_utils/sort-order-then-alpha');
+const sortOrderThenAlpha = require('../_utils/sort-order-then-alpha');
 
-// module.exports = {
-//   navigation: (collection) =>
-//     collection
-//       .getFilteredByGlob('src/*/index.*')
-//       .filter((item) => 'nav' in item.data && 'order' in item.data.nav)
-//       .sort((a, b) => sortOrderThenAlpha(a, b)),
-//   navigation2: (collection) =>
-//     collection
-//       .getFilteredByGlob(['src/galleries/*/index.md'])
-//       .sort((a, b) => sortOrderThenAlpha(a, b)),
-// };
+module.exports = {
+	navigation: (collection) =>
+		collection
+			.getFilteredByGlob('src/pages/*/index.*')
+			.filter((item) => 'nav' in item.data && 'order' in item.data.nav)
+			.sort((a, b) => sortOrderThenAlpha(a, b)),
+	navigation2: (collection) =>
+		collection
+			.getFilteredByGlob(['src/galleries/*/index.md'])
+			.sort((a, b) => sortOrderThenAlpha(a, b)),
+};
