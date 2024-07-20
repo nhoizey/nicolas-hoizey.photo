e68e7487986d2cd364b95efab5e5d49861342654
Nicolas Hoizey <nicolas@hoizey.com>
2024-07-20 08:10:53 +0200

    Fix layout choice

--- src/collections/collections.11tydata.js
+++ src/collections/collections.11tydata.js
@@ -6,9 +6,10 @@ module.exports = {
 				return data.layout;
 			}
 
-			if (data.page.filePathStem.match(/^\/pages\/galleries\/[^\/]+/)) {
-				if (data.page.filePathStem.endsWith('/index')) {
-					return 'gallery';
+			if (data.page.filePathStem.match(/^\/collections\/photos\/[^\/]+/)) {
+				if (process.env.NODE_ENV === 'production') {
+					// There is no photo page in production, they redirect to a gallery photo
+					return false;
 				}
 				return 'photo';
 			}
