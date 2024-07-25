10a6779ea1d59e9f454fa9bdc1956f8f54ced4b5
Nicolas Hoizey <nicolas@hoizey.com>
2024-07-25 12:47:34 +0200

    Define permalinks

--- src/pages/pages.11tydata.js
+++ src/pages/pages.11tydata.js
@@ -5,7 +5,6 @@ module.exports = {
 				// A layout has been set in the content Front Matter
 				return data.layout;
 			}
-
 			if (data.page.filePathStem.match(/^\/pages\/galleries\/[^\/]+/)) {
 				if (data.page.filePathStem.endsWith('/index')) {
 					return 'galleries';
@@ -14,6 +13,18 @@ module.exports = {
 			}
 
 			return 'pages';
+		},
+		permalink: (data) => {
+			if (data.permalink !== undefined && data.permalink !== '') {
+				// A permalink has been set in the content Front Matter
+				return data.permalink;
+			}
+			const path = data.page.filePathStem.replace(/^\/pages/, '');
+			if (path.match(/^\/galleries\/[^\/]+/) && !path.endsWith('/index')) {
+				return path + '/index.html';
+			} else {
+				return path.replace(/^/, '') + '.html';
+			}
 		},
 	},
 };
