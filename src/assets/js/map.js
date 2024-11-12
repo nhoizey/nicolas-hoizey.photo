f40892621e750f5f87da95969f53ee2f0e18fc58
Nicolas Hoizey <nicolas@hoizey.com>
2024-11-12 23:33:21 +0100

    Add geolocation control to the map

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -236,6 +236,19 @@ import polylabel from "polylabel";
 			new mapboxgl.NavigationControl({ showCompass: false }),
 			"top-right",
 		);
+
+		map.addControl(
+			new mapboxgl.GeolocateControl({
+				positionOptions: {
+					enableHighAccuracy: true,
+				},
+				// When active the map will receive updates to the device's location as it changes.
+				trackUserLocation: true,
+				// Draw an arrow next to the location dot to indicate which direction the device is heading.
+				showUserHeading: true,
+			}),
+		);
+
 		map.addControl(new mapboxgl.FullscreenControl());
 
 		const mapStyles = [
