7fc2a0d180ba5a62298cafafab53fcf20b864c64
Nicolas Hoizey <nicolas@hoizey.com>
2025-04-18 23:01:10 +0200

    Manage groups

--- _scripts/posse-to-flickr.js
+++ _scripts/posse-to-flickr.js
@@ -6,7 +6,12 @@ import slugify from "../src/_11ty/_utils/slugify.js";
 import matter from "gray-matter";
 
 import { createFlickr } from "flickr-sdk";
-const { flickr } = createFlickr(process.env.FLICKR_CONSUMER_KEY);
+const { flickr } = createFlickr({
+	consumerKey: process.env.FLICKR_CONSUMER_KEY,
+	consumerSecret: process.env.FLICKR_CONSUMER_SECRET,
+	oauthToken: process.env.FLICKR_OAUTH_TOKEN,
+	oauthTokenSecret: process.env.FLICKR_OAUTH_TOKEN_SECRET,
+});
 
 const FEED_URL = 'https://nicolas-hoizey.photo/feeds/mastodon/photos.json';
 
@@ -42,24 +47,58 @@ const posseToFlickr = async () => {
 		oauthTokenSecret: process.env.FLICKR_OAUTH_TOKEN_SECRET,
 	});
 
-	const photoId = await upload(path.resolve(`src/collections/photos/${photoToPosseSlug}/${photoToPosseSlug}.jpg`));
+	const photoData = await upload(path.resolve(`src/collections/photos/${photoToPosseSlug}/${photoToPosseSlug}.jpg`));
+	const photoId = photoData.id;
 
 	console.log(
-		`Uploaded photo "${photoToPosse.title}" to Flickr: https://www.flickr.com/photos/nicolas-hoizey/${photoId.id}/`
+		`Uploaded photo "${photoToPosse.title}" to Flickr: https://www.flickr.com/photos/nicolas-hoizey/${photoId}/`
 	);
 
 	const photoToPosseMatter = matter.read(
 		`src/collections/photos/${photoToPosseSlug}/index.md`,
 	);
+	// console.dir(photoToPosseMatter, { depth: null });
+
+	const tagsForGroups = photoToPosseMatter.data.tags;
+	tagsForGroups.push(photoToPosseMatter.data.gear.camera.brand);
+	tagsForGroups.push(photoToPosseMatter.data.gear.camera.model);
+	tagsForGroups.push(photoToPosseMatter.data.gear.lenses[0].model);
+	// TODO: also add groups based on categories the photo belongs to
+
 	const groups = [];
-	for (const tag of photoToPosseMatter.data.tags) {
+	for (const tag of tagsForGroups) {
 		switch (tag) {
+			case 'Travel':
+				groups.push('63277308@N00', '460979@N25', '402895@N25', '11488522@N00', '48926546@N00', '88527108@N00', '14730570@N21', '41425956@N00', '74794523@N00', '651467@N20', '2904475@N20', '11427634@N00', '1412034@N24');
+				break;
 			case 'République dominicaine':
 				groups.push('76716807@N00', '79884596@N00', '2150860@N23', '52195003@N00');
 				break;
+			case 'Fujifilm':
+				groups.push('1942023@N20', '1927840@N20', '4001926@N22', '1763261@N25', '14665600@N23', '2756287@N25', '1874432@N24', '1426195@N22', '728696@N23', '1407416@N22', '843996@N23', '1179025@N25', '2765150@N21', '2746396@N25', '2741340@N22', '2140999@N24', '1878068@N23', '93484108@N00');
+				break;
+			case 'X-T2':
+				groups.push('2970680@N25', '2841709@N25', '2983137@N20', '3057090@N21', '4001926@N22', '2925177@N21');
+				break;
+			case 'X-T3':
+				groups.push('4001926@N22');
+				break;
+			case 'Fujinon XF 10-24mm f/4.0 R OIS':
+				groups.push('2077382@N20', '2621709@N24', '2010873@N23', '2654405@N20');
+				break;
 		}
 	}
-	// TODO: also add groups based on categories the photo belongs to
+
+	for (const groupId of groups) {
+		await flickr("flickr.groups.pools.add", {
+			group_id: groupId,
+			photo_id: photoId,
+		}).catch((error) => {
+			console.error(
+				`Error adding photo to group ${groupId}: ${error.message}`,
+			);
+		});
+	}
 
 };
 
