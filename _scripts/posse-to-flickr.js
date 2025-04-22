4fa725fbdd1c425e31fa23d3172a80ea10e4e62d
Nicolas Hoizey <nicolas@hoizey.com>
2025-04-22 19:01:21 +0200

    Add more groups

--- _scripts/posse-to-flickr.js
+++ _scripts/posse-to-flickr.js
@@ -94,6 +94,21 @@ const posseToFlickr = async () => {
 			case 'dominican-republic':
 				groups.add('76716807@N00').add('79884596@N00');
 				break;
+			case 'animal':
+				groups.add('61595479@N00').add('387770@N21').add('47643757@N00').add('632084@N23').add('32733435@N00').add('35025468@N00').add('93779905@N00').add('19126019@N00').add('3601763@N25').add('646385@N24').add('860455@N21').add('647756@N24').add('2719884@N21').add('1286217@N25').add('14702046@N25').add('2122493@N22');
+				break;
+			case 'marine-mammals':
+				groups.add('34724210@N00');
+				break;
+			case 'catacea':
+				groups.add('87972607@N00').add('74159600@N00');
+				break;
+			case 'whale':
+				groups.add('722693@N25').add('23226934@N00').add('1721972@N20');
+				break;
+			case 'wildlife':
+				groups.add('79625603@N00').add('49502993915@N01').add('1780111@N23').add('1744900@N21').add('517805@N22').add('915922@N23').add('2812345@N21').add('2332929@N20').add('2740203@N20').add('1831637@N21').add('2624147@N23').add('755229@N23').add('24376064@N00').add('897629@N21').add('613546@N23').add('49032118@N00').add('1651475@N20').add('579191@N22').add('93538174@N00');
+				break;
 			case 'fujifilm':
 				groups.add('1942023@N20').add('1927840@N20').add('4001926@N22').add('1763261@N25').add('14665600@N23').add('2756287@N25').add('1874432@N24').add('1426195@N22').add('728696@N23').add('1407416@N22').add('843996@N23').add('1179025@N25').add('2765150@N21').add('2746396@N25').add('2741340@N22').add('2140999@N24').add('1878068@N23').add('93484108@N00');
 				break;
@@ -106,16 +121,21 @@ const posseToFlickr = async () => {
 			case 'fujinon-xf-10-24mm-f-4-0-r-ois':
 				groups.add('2077382@N20').add('2621709@N24').add('2010873@N23').add('2654405@N20');
 				break;
+			default:
+				groups.add('3109374@N20').add('14625602@N25').add('14805334@N23');
+				break;
 		}
 	}
 
+	console.info(`
+Trying to add the photo to ${groups.size} groups…`);
 	for (const groupId of groups) {
 		await flickr("flickr.groups.pools.add", {
 			group_id: groupId,
 			photo_id: photoId,
 		}).catch((error) => {
 			console.error(
-				`Error adding photo to group ${groupId}: ${error.message}`,
+				`Error adding photo to group https://flickr.com/groups/${groupId}/: ${error.message}`,
 			);
 		});
 	}
