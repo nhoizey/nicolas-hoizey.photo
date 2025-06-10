308af54e687e7cbf7b2f2f01d23a76b7ac0c4157
Nicolas Hoizey <nicolas@hoizey.com>
2025-06-10 14:13:34 +0200

    Add commented code for buildings

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -89,6 +89,49 @@ const decodeHTML = (html) => {
 
 			map.setTerrain({ source: "mapbox-dem", exaggeration: 1.3 });
 
+			// if (!map.getLayer("3d-buildings")) {
+			// 	const layers = map.getStyle().layers;
+			// 	const labelLayerId = layers.find(
+			// 		(layer) => layer.type === 'symbol' && layer.layout['text-field']
+			// 	).id;
+			// 	map.addLayer(
+			// 		{
+			// 			'id': '3d-buildings',
+			// 			'source': 'composite',
+			// 			'source-layer': 'building',
+			// 			'filter': ['==', 'extrude', 'true'],
+			// 			'type': 'fill-extrusion',
+			// 			'minzoom': 15,
+			// 			'paint': {
+			// 				'fill-extrusion-color': '#aaa',
+
+			// 				// Use an 'interpolate' expression to
+			// 				// add a smooth transition effect to
+			// 				// the buildings as the user zooms in.
+			// 				'fill-extrusion-height': [
+			// 					'interpolate',
+			// 					['linear'],
+			// 					['zoom'],
+			// 					15,
+			// 					0,
+			// 					15.05,
+			// 					['get', 'height']
+			// 				],
+			// 				'fill-extrusion-base': [
+			// 					'interpolate',
+			// 					['linear'],
+			// 					['zoom'],
+			// 					15,
+			// 					0,
+			// 					15.05,
+			// 					['get', 'min_height']
+			// 				],
+			// 				'fill-extrusion-opacity': 0.6
+			// 			}
+			// 		}
+			// 	);
+			// }
+
 			if (!map.getLayer("clusters")) {
 				map.addLayer({
 					id: "clusters",
