592fec74a223eaaf9430b8378d14cb39a178a73c
Nicolas Hoizey <nicolas@hoizey.com>
2025-07-03 15:52:13 +0200

    Add Mastodon instance environment variable

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,4 +1,5 @@
 // biome-ignore lint/correctness/noUnusedImports: dotenv
 import {} from "dotenv/config";
 
+export const MASTODON_INSTANCE = process.env.MASTODON_INSTANCE;
 export const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
